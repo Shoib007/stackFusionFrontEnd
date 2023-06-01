c3238739bbded65a0c49e2fd8920d3e74bca60f6
Shoib007 <skhantanda@gmail.com>
2023-06-02 00:01:52 +0530

    Did some changes in the BaseURL to connect AWS

--- src/BaseURL.jsx
+++ src/BaseURL.jsx
@@ -1,2 +1,2 @@
-export const BaseURL = "http://localhost:8000";
+export const BaseURL = "http://65.0.133.40:8000/";
 
--- src/UserPage.jsx
+++ src/UserPage.jsx
@@ -25,7 +25,7 @@ export default function UserPage() {
         {
           userData.map((user) => {
             return (
-              <div className="card m-2" style={{width: "18rem"}}>
+              <div key={user.id} className="card m-2" style={{width: "18rem"}}>
                 <div className="card-body">
                   <h5 className="card-title">{user.name}</h5>
                   <h6 className="card-subtitle mb-2 text-muted">{user.email}</h6>
